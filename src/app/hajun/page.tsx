1d5297e761bfa9d3ec2aa6ec5f3fc798ddf46414
HaJunRyu <hjr4557@naver.com>
2025-05-04 20:03:06 +0900

    feat: MainThumbnail 영역 UI를 구현한다

--- src/app/hajun/page.tsx
+++ src/app/hajun/page.tsx
@@ -1,9 +1,11 @@
 import Header from '@/components/hajun/Header';
+import Main from '@/components/hajun/Main';
 
 export default function HajunBlogPage() {
   return (
     <div>
       <Header />
+      <Main />
     </div>
   );
 }
---
+++ src/components/hajun/Main.tsx
@@ -0,0 +1,11 @@
+import MainThumbnail from './MainThumbnail';
+
+function Main() {
+  return (
+    <main className="max-w-7xl w-full mx-auto pt-[24px] pb-[100px]">
+      <MainThumbnail />
+    </main>
+  );
+}
+
+export default Main;
