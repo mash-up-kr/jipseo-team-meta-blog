548fad1c3bcb26301cb21ab241f2a376062d2155
jeongwoo903 <www8565@naver.com>
2025-05-04 11:35:24 +0900

    feat: page에 Feed 및 광고 적용

---
+++ src/app/jeongwoo/_components/Advertise.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+const Advertise = () => {
+  return (
+    <div className="max-w-[750px] m-auto py-[15px] bg-[#EBEBED] rounded-[12px] flex flex-col items-center justify-center text-center">
+      <span className="text-[16px] leading-[24px] text-[#797C86]">Advertisement</span>
+      <span className="font-bold text-[24px] leading-[32px] text-[#797C86]">You can place ads</span>
+      <span className="text-[20px] leading-[28px] text-[#797C86]">750x100</span>
+    </div>
+  );
+};
+
+export default Advertise;
